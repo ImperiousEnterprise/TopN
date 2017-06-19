3d8f132247b0b55368be49b8193a76f31753ead8
Adefemi Adeyemi <imperiousenterprise@hotmail.com>
2017-06-19 18:17:00 +0900

    Updated Less to use Greater

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func (h *IntHeap) Peek()(x int){
 
 func (h IntHeap) Len() int           { return len(h) }
 
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
 
 func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
