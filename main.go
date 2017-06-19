02130c4bb98edb2ddc645016f6ef304485d8461a
Adefemi Adeyemi <imperiousenterprise@hotmail.com>
2017-06-19 15:46:54 +0900

    Finished Project. Added buffers to not store so much information into the ram.

--- main.go
+++ main.go
@@ -4,31 +4,20 @@ import (
 	"fmt"
 	"strconv"
 	"container/heap"
+	"os"
+	"bufio"
+	"io"
+	"bytes"
 )
 
 
 func main() {
 	var input string
 
-	fmt.Print("Enter N =")
+	fmt.Print("Enter N for top N numbers =")
 	fmt.Scanf("%s",&input)
 	N, _:= strconv.Atoi(input)
 
-	uh := NewMinHeap(N)
-	//heap.Init(uh)
-
-
-	uh.Push(4)
-	uh.Push(7)
-	uh.Push(5)
-	uh.Push(10)
-
-
-	fmt.Printf("minimum: %d\n", uh.IntHeap[0])
-	for uh.Len() > 0 {
-		fmt.Printf("%d ", heap.Pop(uh))
-	}
-        /**
 	f, err := os.Open("number.txt")
 	if (err != nil){
 		panic(err)
@@ -37,10 +26,11 @@ func main() {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
+	buff := make([]byte, 0, 1024*1024)
+	scanner.Buffer(buff,10*1024*1024)
 	count := 0
 
-
-	heap.Init(h)
+	v := &IntHeap{}
 
 	for scanner.Scan() {
 		s := scanner.Text()
@@ -48,112 +38,96 @@ func main() {
 
 
 
-		if(count < N){
-		//v = append(v, num)
-		heap.Push(h, num)
+		if(count < N && v.notADup(num)){
+		  *v = append(*v, num)
 		}else if(count == N){
-
-
+		  heap.Init(v)
 		}
 
-		if(count > N){
-
+		if(count >= N && v.notADup(num)){
+		  heap.Push(v,num)
 		}
 		count++
-	}**/
+	}
 
+	//Cases when user input is greater than file length
+	if(N > count){
+	   heap.Init(v)
+	}
+	for v.Len() >= 1 {
+
+		fmt.Printf("%d ", heap.Pop(v))
+
+	}
 }
-type Set interface {
-	Add(value int)
-	Contains(value int) (bool)
-	Length() (int)
+
+
+func lineCount(r io.Reader)(int, error){
+	b := make([]byte, 32*1024)
+	co := 1
+
+	lineSep := []byte{'\n'}
+
+	for{
+		c, err := r.Read(b)
+		co += bytes.Count(b[:c], lineSep)
+
+		switch {
+
+		case err == io.EOF: return co, nil
+		case err != nil: return co, err
+
+		}
+	}
 }
-type MinHeap struct {
-	IntHeap []int
-}
-
-
-
 
 
 type IntHeap []int
 
-func (h MinHeap) Len() int           { return len(h.IntHeap) }
-func (h MinHeap) Less(i, j int) bool { return h.IntHeap[i] < h.IntHeap[j] }
-func (h MinHeap) Greater(i, j int) bool { return h.IntHeap[i] > h.IntHeap[j] }
-func (h MinHeap) Swap(i, j int)      { h.IntHeap[i], h.IntHeap[j] = h.IntHeap[j], h.IntHeap[i] }
-func (h *MinHeap) Peek()(x int){
-	old := h.IntHeap
-	n := len(old)
-	x = old[n-1]
+// Peek allows for peek at the smallest number in the heap
+func (h *IntHeap) Peek()(x int){
+	old := *h
+	x = old[0]
 	return x
 }
-func (h *MinHeap) Push(x interface{}) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	if(h.Greater(x.(int),h.Peek())){
-	   h.Pop()
-	   h.bubbleUp()
-	   //*h = append(*h, x.(int))
-	}
-	for _, value := range values {
-		h.list.Add(value)
-	}
-	size := h.Len()/2 + 1
-	for i := size; i >= 0; i-- {
-		h.bubbleDownIndex(i)
-	}
+
+func (h IntHeap) Len() int           { return len(h) }
+
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
 
+// Push before inserts checks for duplicates and removes the smallest number to input the largest,
+func (h *IntHeap) Push(x interface{}) {
+
+
+	if(x.(int) > h.Peek()){
+	   heap.Pop(h)
+	   *h = append(*h, x.(int))
+	}
 
 }
+func (h *IntHeap)notADup(x int) bool{
+	for i := 0; i < h.Len(); i++ {
+		if((*h)[i] == x){
+		   return false
+		}
+	}
+	return true
+}
 
-func (h *MinHeap) Pop() interface{} {
-	old := h.IntHeap
+
+func (h *IntHeap) Pop() interface{} {
+
+	old := *h
+
 	n := len(old)
+
 	x := old[n-1]
-	h.IntHeap = old[0 : n-1]
+
+	*h = old[0 : n-1]
+
 	return x
-}
 
-// Performs the "bubble down" operation. This is to place the element that is at the index
-// of the heap in its correct place so that the heap maintains the min/max-heap order property.
-func (heap *MinHeap) bubbleDownIndex(index int) {
-	size := heap.Len()
-	for leftIndex := index<<1 + 1; leftIndex < size; leftIndex = index<<1 + 1 {
-		rightIndex := index<<1 + 2
-		smallerIndex := leftIndex
-		leftValue := heap.IntHeap[leftIndex]
-		rightValue := heap.IntHeap[rightIndex]
-		if rightIndex < size && heap.Less(leftValue, rightValue){
-			smallerIndex = rightIndex
-		}
-		indexValue := heap.IntHeap[index]
-		smallerValue := heap.IntHeap[smallerIndex]
-		if heap.Greater(indexValue, smallerValue)  {
-			heap.Swap(index, smallerIndex)
-		} else {
-			break
-		}
-		index = smallerIndex
-	}
-}
-
-// Performs the "bubble up" operation. This is to place a newly inserted
-// element (i.e. last element in the list) in its correct place so that
-// the heap maintains the min/max-heap order property.
-func (heap *MinHeap) bubbleUp() {
-	index := heap.Len() - 1
-	for parentIndex := (index - 1) >> 1; index > 0; parentIndex = (index - 1) >> 1 {
-		indexValue := heap.IntHeap[index]
-		parentValue := heap.IntHeap[parentIndex]
-		if heap.Less(parentValue, indexValue) {
-			break
-		}
-		heap.Swap(index, parentIndex)
-		index = parentIndex
-	}
-}
-func NewMinHeap(size int) *MinHeap{
-	return &MinHeap{ IntHeap: make([]int, size)}
 }
